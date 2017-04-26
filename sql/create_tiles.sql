a2b916d1e25b18767cdc8a1ed3ac9314fe9dbd88
Andy Teucher <andy.teucher@gov.bc.ca>
2017-04-26 11:55:13 -0700

    Add Apache header to py and sql files

--- sql/create_tiles.sql
+++ sql/create_tiles.sql
@@ -1,3 +1,16 @@
+-- Copyright 2017 Province of British Columbia
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+-- http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- 
+-- See the License for the specific language governing permissions and limitations under the License.
+
 -- Some marine portions of input data falls outside of the 20k tiles (such as
 -- Conservancies) To fill the gaps, create a tile layer that includes 250k
 -- tiles for marine areas (except for SSOG to avoid confusion with USA border)
