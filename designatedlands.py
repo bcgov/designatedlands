b1ddef9a230f7d997a7c4c16911b662d939b1ce8
Simon Norris <snorris@hillcrestgeo.ca>
2020-09-15 15:05:55 -0700

    remove scan for postgresql://designatedlands:designatedlands@localhost:5433/designatedlands, the config file is available for changing the default parameters

--- designatedlands.py
+++ designatedlands.py
@@ -49,20 +49,12 @@ import pgdata
 LOG = logging.getLogger(__name__)
 
 
-# pick up db_url from $DATABASE_URL if available
-if "DATABASE_URL" in os.environ:
-    db_url = os.environ["DATABASE_URL"]
-else:
-    db_url = (
-        "postgresql://designatedlands:designatedlands@localhost:5432/designatedlands"
-    )
-
 DEFAULT_CONFIG = {
     "dl_path": "source_data",
     "sources_designations": "sources_designations.csv",
     "sources_supporting": "sources_supporting.csv",
     "out_path": "outputs",
-    "db_url": db_url,
+    "db_url": "postgresql://designatedlands:designatedlands@localhost:5432/designatedlands",
     "n_processes": -1,
     "resolution": 10,
 }
